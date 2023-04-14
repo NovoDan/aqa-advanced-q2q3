1c655b00a49aeda4bb0facbbd15f487e63c36eb1
Danylo Novostroinyi <danylo_novostroinyi@epam.com>
2023-04-14 16:23:48 +0300

    edded ability to load properties from environment variables

--- src/main/java/com/epam/novostroinyi/core/config/Configuration.java
+++ src/main/java/com/epam/novostroinyi/core/config/Configuration.java
@@ -1,7 +1,12 @@
 package com.epam.novostroinyi.core.config;
 
 import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.LoadType;
+import org.aeonbits.owner.Config.Sources;
 
+@LoadPolicy(LoadType.MERGE)
+@Sources({"system:env", "file:src/main/resources/secret.properties"})
 public interface Configuration extends Config {
 
   @Key("rp.endpoint")
