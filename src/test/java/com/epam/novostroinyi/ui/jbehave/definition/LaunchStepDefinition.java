2694eb3b10a11ac94113cf079453093077e932ef
Danylo Novostroinyi <danylo_novostroinyi@epam.com>
2023-05-11 15:36:13 +0300

    pre- and postconditions moved to separate class

--- src/test/java/com/epam/novostroinyi/ui/jbehave/definition/LaunchStepDefinition.java
+++ src/test/java/com/epam/novostroinyi/ui/jbehave/definition/LaunchStepDefinition.java
@@ -1,34 +1,19 @@
 package com.epam.novostroinyi.ui.jbehave.definition;
 
 import static com.epam.novostroinyi.core.assertion.Assert.assertHasSize;
-import static com.epam.novostroinyi.core.util.WebDriverUtils.openUrl;
 
-import com.epam.novostroinyi.core.config.Property;
 import com.epam.novostroinyi.core.ui.element.UiElementsCollection;
-import com.epam.novostroinyi.core.util.WebDriverUtils;
 import com.epam.novostroinyi.step.LaunchesSteps;
 import com.epam.novostroinyi.step.LoginPageSteps;
 import com.epam.novostroinyi.step.TestItemsSteps;
 import lombok.SneakyThrows;
-import org.jbehave.core.annotations.AfterScenario;
-import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-public class LaunchStepDefinition {
+public class LaunchStepDefinition extends BaseBddStepDefinition {
 
   private LaunchesSteps launchesSteps;
-
-  @BeforeScenario
-  public void beforeScenario() {
-    openUrl(Property.COMMON_PROPERTY.baseUrl());
-  }
-
-  @AfterScenario
-  public void tearDown() {
-    WebDriverUtils.closeWebDriver();
-  }
 
   @Given("Launch Page is opened")
   public void openLaunchPage() {
@@ -39,7 +24,7 @@ public class LaunchStepDefinition {
   @Given("Launch with id $launchId is opened")
   public void openLaunch(int launchId) {
     UiElementsCollection launchesList = launchesSteps.getLaunchesList();
-    while(launchesList.size() == 0) {
+    while (launchesList.size() == 0) {
       launchesList.refreshElement();
     }
     launchesList.scrollTo(launchId);
@@ -53,6 +38,7 @@ public class LaunchStepDefinition {
 
   @Then("Test Items number after filter is $itemsSize")
   public void checkThatItemsAreFiltered(int itemsSize) {
-    assertHasSize(new TestItemsSteps().getTestItems(), itemsSize, "Expected Items list size is not equal to expected");
+    assertHasSize(new TestItemsSteps().getTestItems(), itemsSize,
+        "Expected Items list size is not equal to expected");
   }
 }
